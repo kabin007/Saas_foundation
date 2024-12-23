33d8fd65165f607adbd9e6ed095c649a220660a9
kabin007 <ghimirekabin060@gmail.com>
2024-12-23 19:09:19 +0545

    made some changes

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY =config("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DEBUG")
+DEBUG = config("DEBUG",cast=bool,default=False)
 
 ALLOWED_HOSTS = [
     ".railway.app" # https://saas.prod.railway.app
